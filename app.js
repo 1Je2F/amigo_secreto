7190b7747551e6d3e42a8076d9dd8e70bf15963d
Jefferson Ferreira <jeff.tads@gmail.com>
2025-03-12 04:43:43 -0300

    entrega da primeira funcionalidade

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Commit 1: Crie um array para armazenar os nomes
+
+let listaNomes = [];
