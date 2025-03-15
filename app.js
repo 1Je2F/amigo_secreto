89e464db74cb30e4d9d0fbb3e508f5f278efca20
1Je2F <jeff.tads@gmail.com>
2025-03-15 04:46:44 -0300

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //Commit 1: Crie um array para armazenar os nomes
 //Commit 2: Implementa uma função para agregar amigos
 //Commit 3: Implementa uma função para atualizar a lista de amigos
+//Commit 4 - Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.
 
 let listaNomes = [];
 
@@ -12,12 +13,36 @@ function adicionarAmigo(amigo) {
     }   else {
         listaNomes.push(nome);
         console.log(listaNomes);
+        
+        redefinirCampo();
+        atualizarLista();
     }
-    
-    redefinirCampo();
 }
 
 function redefinirCampo() {
     document.getElementById('amigo').value = "";
 }
 
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (let i = 0; i < listaNomes.length; i++) {
+        lista.innerHTML += "<li>" + listaNomes[i] + "</li>";
+    }
+}
+
+function sortearAmigo() {
+    if (listaNomes.length === 0) {
+        alert("Por favor, adicione um amigo antes de sortear!");
+    }
+    else {
+        sortearIndice();
+    }
+}
+
+function sortearIndice() {
+        let indice = Math.floor(Math.random() * listaNomes.length);
+        let nomeSorteado = listaNomes[indice];
+        let sorteado = document.getElementById('resultado');
+        sorteado.innerHTML = "O amigo sorteado foi: " + nomeSorteado;
+}
