8e9225ab78169fee82996def4482571bd5dcdc77
Jefferson Ferreira <jeff.tads@gmail.com>
2025-03-15 04:13:01 -0300

    Completo com 3 funcionalidades

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 //Commit 1: Crie um array para armazenar os nomes
 //Commit 2: Implementa uma função para agregar amigos
+//Commit 3: Implementa uma função para atualizar a lista de amigos
+
 let listaNomes = [];
 
 function adicionarAmigo(amigo) {
